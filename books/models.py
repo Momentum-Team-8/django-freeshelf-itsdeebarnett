476f0dc98e77b5ab1d3e61ef7bdaf41e24d865f8
itsdeebarnett <jdeebarnett@gmail.com>
2021-06-21 19:19:19 -0400

    model and admin finished

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -8,3 +9,14 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    book_url = models.URLField()
+    description = models.TextField()
+    created_by = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
